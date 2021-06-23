f6b2bebd8c1ec6f6d19cfcc7d0eddc39b435364b
Gokulaa2311 <78991586+Gokulaa2311@users.noreply.github.com>
2021-06-22 17:55:22 -0700

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAI0wU7igj_7Tb2qINA3L1bLSutgzXBSS8",
+    authDomain: "kwitter-f33a7.firebaseapp.com",
+    databaseURL: "https://kwitter-f33a7-default-rtdb.firebaseio.com",
+    projectId: "kwitter-f33a7",
+    storageBucket: "kwitter-f33a7.appspot.com",
+    messagingSenderId: "540003977234",
+    appId: "1:540003977234:web:58f609909db551813e30cb"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
